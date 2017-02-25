0c3665100cd2a26c042c0562f1b6ab45eca8b8a6
Maxence Maireaux <maxence@realstorm.fr>
2017-02-25 14:53:28 +0100

    mend

--- outputs.tf
+++
@@ -1,7 +0,0 @@
-output "Docker Swarm Manager(s) Public IPs" {
-  value = "${join("\n               ", formatlist("%s", split(",", scaleway_server.swarm_manager.public_ip)))}"
-}
-
-output "Docker Swarm Node(s) Public IPs" {
-  value = "${join("\n               ", formatlist("%s", split(",", scaleway_server.swarm_node.*.public_ip)))}"
-}

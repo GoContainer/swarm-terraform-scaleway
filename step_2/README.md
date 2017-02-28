20e547beef1b136d043c244796c31327eeb37ee2
Maxence Maireaux <maxence@realstorm.fr>
2017-02-28 23:16:46 +0100

    Refacto & Ansible deploy stack

---
+++ step_2/README.md
@@ -0,0 +1,40 @@
+# Cluster Swarm avec Terraform et Scaleway
+
+## Description
+
+Le projet permet en quelques secondes de monter une base d'un cluster swarm avec le provider scaleway et terraform.
+
+## Prérequis
+- Docker
+- Make
+- Un compte Scaleway
+
+## Utilisation
+
+- Scaleway VM = VC1M
+- Nombre min de Manager = 1
+- Nombre mon de Node = 3 
+
+## Installation du projet
+  
+```
+git clone git@github.com:GoContainer/swarm-terraform-scaleway.git
+cd swarm-terraform-scaleway
+cp terraform.tfvars.example terraform.tfvars 
+```
+
+Modifier les valeurs du fichier terraform.tfvars 
+
+## Utilisations 
+
+``make apply`` : Lance la création du cluster swarm selon le fichier de configuration.   
+``make destroy`` : Supprime le cluster créé.  
+``make output`` : Donne l'IP du swarm manager.  
+``make validate`` : Permet de validé les valeurs mise en conf.
+
+## Grafana tempalte 
+
+
+curl --silent --fail --show-error --request POST http://admin:admin@grafana:3000/api/dashboards/import --header "Content-Type: application/json" --data-binary "@namespace/monitoring/grafana/dashboards/Grafana_Dashboard.json"
+
+curl --silent --fail --show-error --request POST http://admin:admin@grafana:3000/api/datasources --header "Content-Type: application/json" --data-binary "@namespace/monitoring/grafana/datasource/prometheus.json"
---
+++ step_2/docker.tf
@@ -0,0 +1,72 @@
+provider "scaleway" {
+  token = "${var.scw_token}"
+  organization = "${var.scw_organization}"
+  region = "${var.scw_region}"
+}
+
+data "scaleway_image" "docker" {
+  architecture = "x86_64"
+  name = "Docker"
+}
+
+resource "scaleway_server" "swarm_manager" {
+  name  = "swarm_manager"
+  image = "${data.scaleway_image.docker.id}"
+  type  = "${var.scw_type_manager}"
+  dynamic_ip_required = true
+
+  volume {
+    size_in_gb = 50
+    type = "l_ssd"
+  }
+
+  connection {
+    user     = "${var.scw_machine_user}"
+    private_key = "${file("${var.scw_ssh_key}")}"
+    agent = false
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "export DEBIAN_FRONTEND=noninteractive",
+      "apt update",
+      "apt upgrade -o Dpkg::Options::='--force-confold' --force-yes -y",
+      "docker swarm  init",
+      "docker swarm join-token -q worker >> /tmp/swarm.token"
+    ]
+  }
+
+  provisioner "local-exec" {
+    command = "scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ${var.scw_ssh_key} ${var.scw_machine_user}@${scaleway_server.swarm_manager.public_ip}:/tmp/swarm.token ."
+  }
+ }
+
+resource "scaleway_server" "swarm_node" {
+  name  = "swarm_node-${count.index + 1}"
+  image = "${data.scaleway_image.docker.id}"
+  type  = "${var.scw_type_node}"
+  count = "${var.swarm_nodes}"
+  dynamic_ip_required = true
+
+  volume {
+    size_in_gb = 50
+    type = "l_ssd"
+  }
+
+  connection {
+    user     = "${var.scw_machine_user}"
+    private_key = "${file("${var.scw_ssh_key}")}"
+    agent = false
+  }
+
+  provisioner "file" {
+    source = "swarm.token"
+    destination = "/tmp/node.token"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "docker swarm join --token $(cat /tmp/node.token) ${scaleway_server.swarm_manager.private_ip}:2377"
+    ]
+  }
+}

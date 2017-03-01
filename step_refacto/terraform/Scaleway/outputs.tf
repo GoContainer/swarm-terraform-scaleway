f7784c6d35584b43450a5eae5f117e7f041cb150
Maxence Maireaux <maxence@realstorm.fr>
2017-03-01 22:31:26 +0100

    Début refacto

---
+++ step_refacto/terraform/Scaleway/outputs.tf
@@ -0,0 +1,7 @@
+output "cluster_node_swarm" {
+  value = "${join("\n", scaleway_server.swarm_node.*.public_ip)}"
+}
+
+output "cluster_master_swarm" {
+  value = "${join("\n", scaleway_server.swarm_manager.*.public_ip)}"
+}
